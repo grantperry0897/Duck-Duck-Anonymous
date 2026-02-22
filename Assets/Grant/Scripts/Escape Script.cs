1f2ef38554465392e2bf7f67624ed22c3d7ef20d
grantperry0897 <grantdsperry@gmail.com>
2026-02-22 04:22:34 -0800

    AA complete

---
+++ Assets/Grant/Scripts/Escape Script.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
